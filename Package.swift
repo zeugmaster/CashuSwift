1d07625ece73e67f958cb96562fa14a456e499ee
zeugmaster <zeugmaster@protonmail.com>
2024-03-12 18:25:44 +0100

    prepare skeleton structure for package

--- Package.swift
+++ Package.swift
@@ -11,11 +11,18 @@ let package = Package(
             name: "cashu-swift",
             targets: ["cashu-swift"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", from: "0.14.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "cashu-swift"),
+            name: "cashu-swift",
+            dependencies: [
+                .product(name: "secp256k1", package: "secp256k1.swift")
+            ]),
+
         .testTarget(
             name: "cashu-swiftTests",
             dependencies: ["cashu-swift"]),
--- Sources/cashu-swift/cashu_swift.swift
+++ Sources/cashu-swift/cashu_swift.swift
@@ -1,2 +1,7 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import secp256k1
+
+class Wallet {
+    
+    
+    
+}
