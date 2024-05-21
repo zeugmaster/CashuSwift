6b06768bde761a8c115ab4915cbcbfd80078d6e8
zeugmaster <zeugmaster@protonmail.com>
2024-05-21 17:31:16 +0200

    encode Tags correctly

--- Sources/cashu-swift/Proof.swift
+++ Sources/cashu-swift/Proof.swift
@@ -37,62 +37,56 @@ enum Tag: Codable {
     case n_sigs(values: [Int])
 
     // Custom decoding
-    enum Tag: Codable {
-        case sigflag(values: [String])
-        case n_sigs(values: [Int])
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
 
-        // Custom decoding
-        init(from decoder: Decoder) throws {
-            var container = try decoder.unkeyedContainer()
-
-            guard let type = try? container.decode(String.self) else {
-                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode type identifier")
-            }
-            
-            switch type {
-            case "sigflag":
-                var values = [String]()
-                while !container.isAtEnd {
-                    let value = try container.decode(String.self)
-                    values.append(value)
-                }
-                self = .sigflag(values: values)
-            case "n_sigs":
-                var values = [Int]()
-                while !container.isAtEnd {
-                    let valueString = try container.decode(String.self)
-                    if let value = Int(valueString) {
-                        values.append(value)
-                    } else {
-                        throw DecodingError.typeMismatch(Int.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Expected string representation of Int, got \(valueString)"))
-                    }
-                }
-                self = .n_sigs(values: values)
-            default:
-                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unknown tag type: \(type)")
-            }
+        guard let type = try? container.decode(String.self) else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode type identifier")
         }
-
-        // Custom encoding
-        func encode(to encoder: Encoder) throws {
-            var container = encoder.unkeyedContainer()
-
-            switch self {
-            case .sigflag(let values):
-                try container.encode("sigflag")
-                for value in values {
-                    try container.encode(value)
-                }
-            case .n_sigs(let values):
-                try container.encode("n_sigs")
-                for value in values {
-                    try container.encode(String(value))
+        
+        switch type {
+        case "sigflag":
+            var values = [String]()
+            while !container.isAtEnd {
+                let value = try container.decode(String.self)
+                values.append(value)
+            }
+            self = .sigflag(values: values)
+        case "n_sigs":
+            var values = [Int]()
+            while !container.isAtEnd {
+                let valueString = try container.decode(String.self)
+                if let value = Int(valueString) {
+                    values.append(value)
+                } else {
+                    throw DecodingError.typeMismatch(Int.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Expected string representation of Int, got \(valueString)"))
                 }
             }
+            self = .n_sigs(values: values)
+        default:
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unknown tag type: \(type)")
         }
     }
 
+    // Custom encoding
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+
+        switch self {
+        case .sigflag(let values):
+            try container.encode("sigflag")
+            for value in values {
+                try container.encode(value)
+            }
+        case .n_sigs(let values):
+            try container.encode("n_sigs")
+            for value in values {
+                try container.encode(String(value))
+            }
+        }
+    }
 }
+
 
 struct SpendingCondition: Codable {
     let nonce: String
