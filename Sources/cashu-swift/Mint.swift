f4bbda5e0329236dccd7dff431515cb399d20755
Dario <zeugmaster@protonmail.com>
2024-08-24 22:43:00 +0200

    make mint codable

--- Sources/cashu-swift/Mint.swift
+++ Sources/cashu-swift/Mint.swift
@@ -9,8 +9,7 @@ import Foundation
 
 /// This is the mint object.
 
-public struct Mint:  Hashable {
-    
+public struct Mint:  Hashable, Codable {
     
     public let url: URL
     var keysets: [Keyset]
