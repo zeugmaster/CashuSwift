9cce4d96b17044a46b37046142e64d72c9fae025
Dario <zeugmaster@protonmail.com>
2025-02-04 18:46:46 +0100

    remove unused code

--- Sources/cashu-swift/Mint.swift
+++ Sources/cashu-swift/Mint.swift
@@ -13,52 +13,9 @@ extension CashuSwift {
         
         public var url: URL
         public var keysets: [Keyset]
-//        public var info: MintInfo?
-//        public var nickname: String?
-        
         public static func == (lhs: Mint, rhs: Mint) -> Bool {
             lhs.url == rhs.url
         }
-        
-//        required public init(from decoder: Decoder) throws {
-//            let container = try decoder.container(keyedBy: CodingKeys.self)
-//            self.url = try container.decode(URL.self, forKey: .url)
-//            self.keysets = try container.decode([Keyset].self, forKey: .keysets)
-//            
-//            let infoContainer = try container.superDecoder(forKey: .info)
-//            if let info = try? MintInfo0_16(from: infoContainer) {
-//                self.info = info
-//            } else if let info = try? MintInfo0_15(from: infoContainer) {
-//                self.info = info
-//            } else if let info = try? MintInfo(from: infoContainer) {
-//                self.info = info
-//            } else {
-//                logger.warning("Could not initiate from decoder mint info of \(self.url.absoluteString) as any known version.")
-//            }
-//            
-//            self.nickname = try container.decodeIfPresent(String.self, forKey: .nickname)
-//        }
-//        
-//        enum CodingKeys: CodingKey {
-//            case url
-//            case keysets
-//            case info
-//            case nickname
-//        }
-//        
-//        public func encode(to encoder: Encoder) throws {
-//            var container = encoder.container(keyedBy: CodingKeys.self)
-//            try container.encode(self.url, forKey: .url)
-//            try container.encode(self.keysets, forKey: .keysets)
-//            
-//            if let info = self.info {
-//                let infoContainer = container.superEncoder(forKey: .info)
-//                try info.encode(to: infoContainer)
-//            }
-//            
-//            try container.encodeIfPresent(self.nickname, forKey: .nickname)
-//        }
-        
         public func hash(into hasher: inout Hasher) {
             hasher.combine(url)
         }
