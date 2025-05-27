982601e2fe9a67b1e7c2329885fde6bcc5baacfa
Dario <zeugmaster@protonmail.com>
2025-05-27 20:19:41 +0200

    remove comments

--- Sources/cashu-swift/Operations/swap.swift
+++ Sources/cashu-swift/Operations/swap.swift
@@ -21,93 +21,6 @@ extension CashuSwift {
                             seed:String? = nil,
                             preferredReturnDistribution:[Int]? = nil) async throws -> (new:[ProofRepresenting],
                                                                                        change:[ProofRepresenting]) {
-//        let fee = try calculateFee(for: proofs, of: mint)
-//        let proofSum = sum(proofs)
-//        
-//        let returnAmount:Int
-//        let changeAmount:Int
-//        
-//        if let amount {
-//            if proofSum >= amount + fee {
-//                returnAmount = amount
-//                changeAmount = proofSum - returnAmount - fee
-//            } else {
-//                throw CashuError.insufficientInputs("SWAP: sum of proofs (\(proofSum)) is less than amount (\(amount)) + fees (\(fee))")
-//            }
-//        } else {
-//            returnAmount = proofSum - fee
-//            changeAmount = 0
-//        }
-//        
-//        // the number of units from potentially mutliple keysets across input proofs must be 1:
-//        // less than 1 would mean no matching keyset/unit
-//        // more than one would imply multiple unit input proofs, which is not supported
-//        
-//        let units = try units(for: proofs, of: mint)
-//        
-//        guard units.count == 1 else {
-//            throw CashuError.unitError("Proofs to swap are either of mixed unit or foreign to this mint.")
-//        }
-//        
-//        guard let activeKeyset = activeKeysetForUnit(units.first!, mint: mint) else {
-//            throw CashuError.noActiveKeysetForUnit("no active keyset could be found for unit \(String(describing: units.first))")
-//        }
-//        
-//        // TODO: implement true output selection
-//        
-//        let swapDistribution = CashuSwift.splitIntoBase2Numbers(returnAmount)
-//        let changeDistribution:[Int]
-//        
-//        if let preferredReturnDistribution {
-//            // TODO: CHECK THAT AMOUNTS ARE ONLY VALID INTEGERS
-//            let preferredReturnDistributionSum = preferredReturnDistribution.reduce(0, +)
-//            guard preferredReturnDistribution.reduce(0, +) == changeAmount else {
-//                throw CashuError.preferredDistributionMismatch(
-//                """
-//                preferredReturnDistribution does not add up to expected change amount.
-//                proof sum: \(proofSum), return amount: \(returnAmount), change amount: \(changeAmount), fees: \(fee), preferred distr sum: \(preferredReturnDistributionSum)
-//                """)
-//            }
-//            changeDistribution = preferredReturnDistribution
-//        } else {
-//            changeDistribution = CashuSwift.splitIntoBase2Numbers(changeAmount)
-//        }
-//        
-//        let combinedDistribution = (swapDistribution + changeDistribution).sorted()
-//        
-//        let deterministicFactors:(String, Int)?
-//        if let seed {
-//            deterministicFactors = (seed, activeKeyset.derivationCounter)
-//        } else {
-//            deterministicFactors = nil
-//        }
-//        
-//        let (outputs, bfs, secrets) = try Crypto.generateOutputs(amounts: combinedDistribution,
-//                                                                 keysetID: activeKeyset.keysetID,
-//                                                                 deterministicFactors: deterministicFactors)
-//        
-////        let internalProofs = normalize(proofs)
-//        let internalProofs = stripDLEQ(proofs)
-//                
-//        let swapRequest = SwapRequest(inputs: internalProofs, outputs: outputs)
-//        let swapResponse = try await Network.post(url: mint.url.appending(path: "/v1/swap"),
-//                                                  body: swapRequest,
-//                                                  expected: SwapResponse.self)
-//        
-//        var newProofs = try Crypto.unblindPromises(swapResponse.signatures,
-//                                                   blindingFactors: bfs,
-//                                                   secrets: secrets,
-//                                                   keyset: activeKeyset)
-//        
-//        var sendProofs = [Proof]()
-//        for n in swapDistribution {
-//            if let index = newProofs.firstIndex(where: {$0.amount == n}) {
-//                sendProofs.append(newProofs[index])
-//                newProofs.remove(at: index)
-//            }
-//        }
-//        
-//        return (sendProofs, newProofs)
         
         let inputs = proofs.map({ Proof($0) })
         let mint = Mint(mint)
@@ -220,7 +133,6 @@ extension CashuSwift {
                                                                  keysetID: activeKeyset.keysetID,
                                                                  deterministicFactors: deterministicFactors)
         
-//        let internalProofs = normalize(proofs)
         let internalProofs = stripDLEQ(inputs)
                 
         let swapRequest = SwapRequest(inputs: internalProofs, outputs: outputs)
@@ -241,7 +153,6 @@ extension CashuSwift {
             }
         }
         
-        // check output proofs dleq
         let outputDLEQ = try Crypto.checkDLEQ(for: sendProofs + changeProofs, with: mint)
         
         return (sendProofs, changeProofs, inputDLEQ, outputDLEQ)
